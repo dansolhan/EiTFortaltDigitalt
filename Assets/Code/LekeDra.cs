2ae87742d05e0e6a7c3d039936c31f5f92683cf4
dansolhan <homenerd@gmail.com>
2015-03-04 11:23:52 +0100

    Masse nye forandringer!

---
+++ Assets/Code/LekeDra.cs
@@ -0,0 +1,150 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class LekeDra : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler {
+	
+	private Vector2 pointerOffset;
+	private RectTransform canvasRectTransform;
+	private RectTransform panelRectTransform;
+	public PanelControl panelControl;
+	public Vector3 defaultPosition;
+	public float smooth;
+	private bool isDragged;
+	private string destination;
+
+
+	Vector3 awayRight = new Vector3 (2000, 0, 1);
+	Vector3 awayLeft = new Vector3 (-2000, 0,1);
+	Vector3 middle = new Vector3(1,1,1);
+
+	Vector3 rightEdge = new Vector3(400f, 0f,0f);
+	Vector3 leftEdge = new Vector3(-400f, 0f,0f);
+	Vector3 PopupEdgeLeft = new Vector3 (200, 0,1);
+	Vector3 PopupEdgeRight = new Vector3 (1800, 0,1);
+	bool isActive;
+
+
+	private Vector3 from;
+	private Vector3 to;
+
+	private PanelControl panelController;
+
+	
+	void Awake () {
+		setActive (false);
+		panelController = (PanelControl) this.transform.parent.GetComponent ("PanelControl");
+
+
+
+
+		Canvas canvas = GetComponentInParent <Canvas>();
+		if (canvas != null) {
+			canvasRectTransform = canvas.transform as RectTransform;
+			panelRectTransform = transform as RectTransform;
+		}
+		to = panelRectTransform.localPosition;
+	}
+
+	void Update() {
+		if (!isDragged) {performMove();}
+	}
+
+	public void moveToCenter() {
+		Debug.Log ("Moving to Center!");
+		to = middle;
+	}
+
+	public void appearFromLeft() {
+		Debug.Log ("Appearing from Left!");
+		panelRectTransform.localPosition = awayLeft;
+		to = middle;
+	}
+
+	public void appearFromRight() {
+		panelRectTransform.localPosition = awayRight;
+		to = middle;
+	}
+
+
+	public void exitRight() {
+		Debug.Log("Appearing to the Right!");
+		to = awayRight;
+	}
+
+	public void exitLeft() {
+		Debug.Log ("Appearing to the Left!");
+		to = awayLeft;
+	}
+
+	public void goToLeftQueue() {
+
+	}
+
+	public void OnPointerDown (PointerEventData data) {
+		isDragged = true;
+		panelRectTransform.SetAsLastSibling ();
+		RectTransformUtility.ScreenPointToLocalPointInRectangle (panelRectTransform, data.position, data.pressEventCamera, out pointerOffset);
+
+	}
+
+	public void OnPointerUp (PointerEventData data) {
+		isDragged = false;
+ 		Vector3 myPos = panelRectTransform.localPosition;
+
+		if (panelRectTransform.localPosition.x > rightEdge.x) {
+						exitRight ();
+						panelController.nextSlide ();
+				} else if (panelRectTransform.localPosition.x < leftEdge.x) {
+						exitLeft ();
+						panelController.previousSlide ();
+
+				} else {
+						if (this.isActive) {
+						//		moveToCenter ();
+						}
+				}
+
+		}
+
+
+	public void OnDrag (PointerEventData data) {
+		float oldPos = canvasRectTransform.position.y;
+		if (panelRectTransform == null) {
+				return;
+				}
+		
+		Vector2 localPointerPosition;
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle (
+			canvasRectTransform, data.position, data.pressEventCamera, out localPointerPosition
+			)) {
+
+			Vector3 newPosition = localPointerPosition - pointerOffset;
+			newPosition.y = oldPos;
+			panelRectTransform.localPosition = newPosition;
+		}
+	}
+
+	private void performMove() {
+		panelRectTransform.localPosition = Vector3.Lerp(panelRectTransform.localPosition, to, smooth * Time.deltaTime);
+	
+	}
+
+	public void setActive(bool active) {
+		this.isActive = active;
+
+	}
+
+	public bool getActive() {
+		return isActive;
+	}
+
+
+
+
+
+
+}
---
+++ Assets/Code/PanelControl.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PanelControl : MonoBehaviour {
+	
+	List<Transform> children;
+	int index = 0;
+	public LekeDra currentChild;
+	public AudioClip onSlideSound;
+	private AudioSource audioSource; 
+
+	// Use this for initialization
+	void Start () {
+		audioSource = (AudioSource) this.GetComponent ("AudioSource");
+		children = new List<Transform>();
+		Debug.Log(transform.childCount.ToString());
+
+		foreach (Transform child in transform) {
+			children.Add(child);
+			Debug.Log(child.name);
+		}
+
+		currentChild = (LekeDra) children [index].gameObject.GetComponent ("LekeDra");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void getSlide(string side) {
+		audioSource.PlayOneShot (onSlideSound, 100f);
+		currentChild.setActive(false);
+		LekeDra lekeDraObject = (LekeDra) children[index].gameObject.GetComponent ("LekeDra");
+		currentChild = lekeDraObject;
+		currentChild.setActive (true);
+		if (side == "right") {
+			currentChild.appearFromRight();
+				}
+		if (side == "left") {
+			currentChild.appearFromLeft();
+				}
+	}
+
+
+	void Center() {
+
+
+	}
+
+	public void nextSlide() {
+		if (index < children.Count -1) {
+				index += 1;
+				getSlide ("left");
+			} else {
+			index = 0;
+			getSlide ("left");
+				}
+
+	}
+
+	public void previousSlide() {
+		if (index > 0) {
+			index -= 1;
+			getSlide ("right");
+				}
+		else {
+			index = children.Count -1;
+			getSlide("right");
+		}
+	}
+
+
+
+
+}
